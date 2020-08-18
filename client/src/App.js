4a7a35ea0b8bd893e3f15f92bfda1bd9762785bf
Eclismic <milcokats@gmail.com>
2020-08-18 15:46:28 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Frontpage from './Components/Frontpage.js'
+
+const App = () =>{
+  return (
+    <div className="App">
+      <Frontpage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Frontpage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Frontpage = () => {
+    return (
+        <div className="frontpage-wrapper">
+            <div className="frontpage-s1">
+                <div className="nav-wrapper">
+
+                </div>
+                <div className="title-wrapper">
+                    <h4>KnipJeThuis</h4>
+                    <p>Voeg jouw knipbeurt toe en zie we er tijd voor je heeft!</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Frontpage
